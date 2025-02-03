d4a1c6bfb1716bb49ca407e670bc60beea2020d2
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2025-02-03 19:48:05 +0100

    settings: build the api_url from base_api_url and base_api_port environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
 declare const process: {
   env: {
     BASE_API_URL: string;
+    BASE_API_PORT: string;
   };
 };
 
 export const environment = {
   production: true,
-  api_url: process.env.BASE_API_URL, // Placeholder for backend URL
+  api_url: `http://${process.env.BASE_API_URL}:${process.env.BASE_API_PORT}`, // Placeholder for backend URL
 };
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const webpack = require('webpack');
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.BASE_API_URL': JSON.stringify(process.env.BASE_API_URL || 'http://localhost:8889'),
+      'process.env.BASE_API_URL': JSON.stringify(process.env.BASE_API_URL || 'localhost'),
+      'process.env.BASE_API_PORT': JSON.stringify(process.env.BASE_API_PORT || '8000'),
     }),
   ],
 };
